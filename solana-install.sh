8a3c6bba1babd7e641c214e4629e5a060e26cf4c
Skid Vis <skid@skidvis.com>
2022-03-28 20:27:28 -0500

    installs ts_node

--- solana-install.sh
+++ solana-install.sh
@@ -4,6 +4,7 @@ source $HOME/.cargo/env
 sh -c "$(curl -sSfL https://release.solana.com/v1.10.4/install)"
 export PATH="/home/ubuntu/.local/share/solana/install/active_release/bin:$PATH"
 sudo npm install -g mocha
+sudo npm install -g ts_node
 cargo install --git https://github.com/project-serum/anchor anchor-cli --locked
 solana --version
 solana config set --url localhost
